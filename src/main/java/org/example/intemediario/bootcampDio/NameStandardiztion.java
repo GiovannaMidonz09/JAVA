28225c9e3cdb268252d63fd772c6fde187065c61
giovanna midonz <gmidonz09@outlook.com>
2026-03-15 22:35:20 -0300

    Update: Bootcamp Dio - DESAFIO 2 (null e isEmpty())

---
+++ src/main/java/org/example/intemediario/bootcampDio/NameStandardiztion.java
@@ -0,0 +1,26 @@
+package org.example.intemediario.bootcampDio;
+
+import java.util.Scanner;
+
+public class NameStandardiztion {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String nomeProjeto = scanner.nextLine();
+
+        // TODO: Remova espaços extras do início e fim da string
+        String nomeProjetoSemEspacos = nomeProjeto.trim();
+
+        // Dica: Use um método da classe String para eliminar espaços em branco antes de validar
+
+        // Se o nome está vazio após o tratamento, imprima "INVALIDO"
+
+            if(nomeProjetoSemEspacos != null && !nomeProjetoSemEspacos.isEmpty() ){
+                System.out.println(nomeProjetoSemEspacos.toUpperCase());
+            }else{
+                System.out.println("INVALIDO");
+            }
+
+
+        // Caso contrário, imprima o nome em letras maiúsculas
+    }
+}
